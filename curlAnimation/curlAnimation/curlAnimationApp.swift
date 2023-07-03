2690e45b5bbf15dd536b2d5c48c1cff02589f935
abhishek2513 <abhishek.dogra25130@gmail.com>
2023-07-03 22:17:26 +0530

    curl animation

---
+++ curlAnimation/curlAnimation/curlAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  curlAnimationApp.swift
+//  curlAnimation
+//
+//  Created by Abhishek Dogra on 03/07/23.
+//
+
+import SwiftUI
+
+@main
+struct curlAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
