9b37b1568adebab131a865095215deaded211327
Abhishek Dogra <abhishekdogra@Abhisheks-MacBook-Pro.local>
2023-09-14 07:11:45 +0530

    grid animation

---
+++ GridAnimation/GridAnimation/GridAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridAnimationApp.swift
+//  GridAnimation
+//
+//  Created by Abhishek Dogra on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct GridAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
