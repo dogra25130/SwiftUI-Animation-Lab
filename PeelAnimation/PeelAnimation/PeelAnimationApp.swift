ce1991b80d8bac225acd880a8bff910f983bad7a
Abhishek Dogra <abhishek.dogra25130@gmail.com>
2023-09-15 07:46:10 +0530

    peel animation

---
+++ PeelAnimation/PeelAnimation/PeelAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeelAnimationApp.swift
+//  PeelAnimation
+//
+//  Created by Abhishek Dogra on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PeelAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
