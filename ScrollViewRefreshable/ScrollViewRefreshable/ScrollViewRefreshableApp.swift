ab71e9fb7e505980e8c1b52ba69af0b9c12c6822
Abhishek Dogra <abhishek.dogra25130@gmail.com>
2023-09-20 06:40:36 +0530

    scrollable

---
+++ ScrollViewRefreshable/ScrollViewRefreshable/ScrollViewRefreshableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewRefreshableApp.swift
+//  ScrollViewRefreshable
+//
+//  Created by Abhishek Dogra on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewRefreshableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
