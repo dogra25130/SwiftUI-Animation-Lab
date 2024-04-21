14cbbccdda15c8ae406daec3f71ed5aa12a2f2a6
Abhishek Dogra <abhishek@loomy.com>
2024-04-21 15:43:50 +0530

    flip animation

---
+++ FlipTransition/FlipTransition/FlipTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipTransitionApp.swift
+//  FlipTransition
+//
+//  Created by Abhishek Dogra on 21/04/24.
+//
+
+import SwiftUI
+
+@main
+struct FlipTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestSwiftUI/TestSwiftUI/TestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUIApp.swift
+//  TestSwiftUI
+//
+//  Created by Abhishek Dogra on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
